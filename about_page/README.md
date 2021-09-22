c75be4ed626e09be17e67ff23655f59689147ebb
Lauren Gustafson <lauren.gustafson17@gmail.com>
2021-09-22 15:37:51 -0700

    css

---
+++ about_page/README.md
@@ -0,0 +1,5 @@
+background: #0d0f19
+
+text: 
+white: #ffffff
+blue: #1f2a77
--- about_page/src/components/AboutLanding/AboutLanding.js
+++ about_page/src/components/AboutLanding/AboutLanding.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import AboutUs from '../AboutUs/AboutUs';
+import './AboutLanding.css'
 
 const AboutLanding = () => {
     return (
         <>
         <h1>Hello from Landing</h1>
+        <div className='about_us'>
+            <AboutUs />
+        </div>
+        <div className='origins'>
+            <h2>Origins</h2>
+        </div>
+        <div className='employee_container'>
+            <h2>Employee Divs</h2>
+        </div>
+        <div className='growing_team'>
+            <h2>Image</h2>
+            <h2>What We're Made Of</h2>
+        </div>
         </>
     )
 }
